62d7093cf3fbe0665be989a1b1b0180d02d34731
kvhong <2217374319@qq.com>
2018-09-09 15:26:43 +0800

    first commit

---
+++ src/wc/wcGUI.java
@@ -0,0 +1,45 @@
+package wc;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class wcGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					wcGUI window = new wcGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public wcGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(300, 50, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+
+}
